daee1e3083fb538f0d3c12c1a4820c5692d5627c
Conor Woods <conorw@gmail.com>
2024-06-04 17:06:36 +0100

    page title

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,9 +7,9 @@ import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
-  title: 'Invoices | Acme Dashboard',
+  title: 'Invoices',
 };
 export default async function Page({
   searchParams,
