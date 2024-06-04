e4f74cc9f8d56e123219012dd49dfc1fe067ff54
Conor Woods <conorw@gmail.com>
2024-06-04 16:14:11 +0100

    not found page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -39,7 +39,6 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
